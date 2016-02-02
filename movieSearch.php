d1712fd24a6e0e2d329f666329ba8e7de004a74b
Stephie <sgan@csumb.edu>
2016-02-02 09:40:21 -0800

    Added search bars

--- movieSearch.php
+++ movieSearch.php
@@ -4,8 +4,8 @@
 	function getMovies() {
 		global $conn;
 		$sql = "SELECT movieID, title 
-				FROM movies
-				ORDER BY title ASC";
+			FROM movies
+			ORDER BY title ASC";
 		$stmt = $conn->prepare($sql);
 		$stmt -> execute();
 		return $stmt -> fetchAll();
@@ -14,8 +14,28 @@
 	function getDirectors() {
 		global $conn;
 		$sql = "SELECT directorID, director
-				FROM movie_director
-				ORDER BY director ASC";
+			FROM movie_director
+			ORDER BY director ASC";
+		$stmt = $conn->prepare($sql);
+		$stmt -> execute();
+		return $stmt -> fetchAll();
+	}
+	
+	function getGenres() {
+		global $conn;
+		$sql = "SELECT genreID, genre
+			FROM movie_genre
+			ORDER BY genre ASC";
+		$stmt = $conn->prepare($sql);
+		$stmt -> execute();
+		return $stmt -> fetchAll();
+	}
+	
+	function getYears() {
+		global $conn;
+		$sql = "SELECT yearID, year
+			FROM movie_year
+			ORDER BY year ASC";
 		$stmt = $conn->prepare($sql);
 		$stmt -> execute();
 		return $stmt -> fetchAll();
@@ -30,9 +50,10 @@
 	<link rel="stylesheet" type="text/css" href="ourStyles.css">
 </head>
 <body>
+	<div id="header"> TESTING </div>
 	
 	<table id="main">
-		<tr><td colspan="2">
+		<tr><td colspan="3">
 			<h2>Choose a Movie Title!</h2>
 			<form method="post">
 				<select size=1 name="movieID">
@@ -64,21 +85,40 @@
 				<input type="submit" value="Search">
 			</form>
 		</td>
-		
+
 		<td>
 			<h2>Search By Genre!</h2>
 			<form method="post">
 				<select size=1 name="genreID">
 					<option value="-1">Choose A Genre</option>
 					<?php
-						//insert code
+						$movieGenres = getGenres();
+						foreach ($movieGenres as $genre) {
+							echo "<option value='" . $genre['genreID'] . "'>" . $genre['genre'] . "</option>";
+						}
 					?>
 				</select>
 				<input type="submit" value="Search">
 			</form>
-		</td></tr>
+		</td>
+		
+		<td>
+			<h2>Search By Year!</h2>
+			<form method="post">
+				<select size="1" name="yearID">
+					<option value="-1">Choose A Year</option>
+					<?php
+						$movieYears = getYears();
+						foreach ($movieYears as $year) {
+							echo "<option value='" . $year['yearID'] . "'>" . $year['year'] . "</option>";
+						}
+					?>
+				</select>
+			</form>
+		</td>
+		</tr>
 	</table>
-	
+
 </body>
 </html>
 
